42edfd0eb407ff0cf1bdd94ea678e23f0610a950
CodinGitHub <gms2programador@gmail.com>
2022-06-16 22:02:58 -0500

    desktop view

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const profile = document.querySelector('.card__profile');
-const shareIcons = document.querySelector('.card__share-icons');
-const shareBtn = document.querySelector('.card__button');
+const profile = document.querySelector('.card__content__profile');
+const shareIcons = document.querySelector('.card__content__share-icons');
+const shareBtn = document.querySelector('.card__content__button');
 
 shareBtn.addEventListener('click', ()=>{
 	console.log('click')
